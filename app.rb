43407ec753f4a3d129a188c77aedfa926e96d7fd
mswopinski <mswopinski@gmail.com>
2020-02-25 19:16:41 +0000

    added weather api key

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "c52ab0514053f3ddcbd3c5d31866ae23" #"YOUR-API-KEY"
+ForecastIO.api_key = "c52ab0514053f3ddcbd3c5d31866ae23"
 
 get "/" do
   # show a view that asks for the location
